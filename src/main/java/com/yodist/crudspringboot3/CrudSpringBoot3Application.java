3aef59f74de0e9cda378738944f50b74cfcb2826
Yodi Satriani <yodisatriani@live.com>
2023-08-09 21:59:40 +0700

    chore: initial commit

---
+++ src/main/java/com/yodist/crudspringboot3/CrudSpringBoot3Application.java
@@ -0,0 +1,13 @@
+package com.yodist.crudspringboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringBoot3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringBoot3Application.class, args);
+	}
+
+}
